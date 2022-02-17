8791e88ee85b8feeebf8b6c0c5680cf5ed434bf2
Luis Gustavo Marini <luisgustavomarini@gmail.com>
2022-02-17 18:13:41 -0300

    refactor

--- lib/pages/animations/animações_controladas.dart
+++ lib/pages/animations/animações_controladas.dart
@@ -21,48 +21,40 @@ class ExpandedItem {
   });
 }
 
-class _AnimacaoControladaPageState extends State<AnimacaoControladaPage> with TickerProviderStateMixin {
-
-  static const String content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vel condimentum eros. Sed non ligula rhoncus, eleifend ante non, congue urna. Sed sed nisi vitae mi ullamcorper interdum. Pellentesque scelerisque ornare justo ac dictum. Fusce molestie erat id rhoncus consectetur. Nullam eu fermentum odio. Aliquam tristique Sed ultrices, ipsum id fermentum cursus, leo eros im";
-
+class _AnimacaoControladaPageState extends State<AnimacaoControladaPage>
+    with TickerProviderStateMixin {
+  static const String content =
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vel condimentum eros. Sed non ligula rhoncus, eleifend ante non, congue urna. Sed sed nisi vitae mi ullamcorper interdum. Pellentesque scelerisque ornare justo ac dictum. Fusce molestie erat id rhoncus consectetur. Nullam eu fermentum odio. Aliquam tristique Sed ultrices, ipsum id fermentum cursus, leo eros im";
 
   late AnimationController controller;
   late Animation<double> _heightFactorAnimation;
   late Animation<double> _rotateAnimation;
 
-
   List<ExpandedItem> item = [];
-
 
   @override
   void initState() {
     super.initState();
 
-    item.addAll(
-      List.generate(
-          51, (index) {
-        controller = AnimationController(
-          vsync: this,
-          duration: const Duration(milliseconds: 300),
-        );
-        _heightFactorAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
-          controller,
-        );
-        _rotateAnimation = Tween<double>(begin: 0.0, end: pi).animate(
-          controller,
-        );
+    item.addAll(List.generate(51, (index) {
+      controller = AnimationController(
+        vsync: this,
+        duration: const Duration(milliseconds: 300),
+      );
+      _heightFactorAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
+        controller,
+      );
+      _rotateAnimation = Tween<double>(begin: 0.0, end: pi).animate(
+        controller,
+      );
 
-         return ExpandedItem(
-           controller: controller,
-           heightFactorAnimation: _heightFactorAnimation,
-           rotateAnimation: _rotateAnimation,
-         );
-        }
-      )
-    );
-
+      return ExpandedItem(
+        controller: controller,
+        heightFactorAnimation: _heightFactorAnimation,
+        rotateAnimation: _rotateAnimation,
+      );
+    }));
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -97,13 +89,12 @@ class _AnimacaoControladaPageState extends State<AnimacaoControladaPage> with Ti
                     ClipRect(
                       child: Align(
                         heightFactor: item[index].heightFactorAnimation.value,
-                        child: Column(
-                               children: const [
-                                FlutterLogo(
-                                  size: 60,
-                                ),
-                                 Text(
-                               content,
+                        child: Column(children: const [
+                          FlutterLogo(
+                            size: 60,
+                          ),
+                          Text(
+                            content,
                           ),
                         ]),
                       ),
